f5c0f802ba0ca55a4e10d55804573d9d042cbfe9
trottola <gtricarico92@gmail.com>
2017-10-10 12:52:34 +0200

    chat ready

--- App/Components/Chat.js
+++ App/Components/Chat.js
@@ -5,23 +5,70 @@ import {
   Text,
   View,
   Button,
+  FlatList,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
+import {send, subscribe } from 'react-native-training-chat-server';
+import RevesedFlatList from 'react-native-reversed-flat-list';
+
+const CHANNEL = 'Problemi';
+
 export default class Chat extends Component <{}> {
+  state= {
+    typing: "",
+    messages: [],
+  };
   static navigationOptions = {
     title: 'Chat',
   };
+
+componentDidMount (){
+  subscribe(CHANNEL, messages => {
+    this.setState({messages});
+  });
+}
+
+async sendMessage(){
+  await send({
+    channel: CHANNEL,
+    sender: this.props.navigation.state.params.name,
+    message: this.state.typing
+  });
+
+  this.setState({
+    typing:'',
+  });
+}
+
+renderItem({item}) {
+  return(
+    <View style={styles.row}>
+        <Text style={styles.sender}>{item.sender}</Text>
+        <Text style={styles.message}>{item.message}</Text>
+    </View>
+  );
+}
 
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome {this.props.navigation.state.params.name}
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
+      <RevesedFlatList data={this.state.messages} renderItem= {this.renderItem}/>
+      <View style={styles.footer}>
+        <TextInput
+            value={this.state.typing}
+            onChangeText={text => this.setState({typing: text})}
+            style={styles.input}
+            autoCapitalize = "sentences"
+            underlineColorAndroid="transparent"
+            placeholder="Type here"
+        />
+        <TouchableOpacity onPress={this.sendMessage.bind(this)}>
+          <Text style={styles.send}>Send</Text>
+        </TouchableOpacity>
+      </View>
       </View>
     );
   }
@@ -30,18 +77,34 @@ export default class Chat extends Component <{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  message: {
+    fontSize: 18,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  row: {
+    padding: 20,
+    borderBottomWidth:1 ,
+    borderBottomColor: '#eee',
+  },
+  sender: {
+    fontWeight: 'bold',
+    paddingRight: 10,
+  },
+  footer: {
+    flexDirection:'row',
+    backgroundColor: '#eee',
+  },
+  input: {
+    flex:1,
+    paddingHorizontal: 20,
+    paddingVertical:10,
+    fontSize:18,
+  },
+  send: {
+    alignSelf: 'center',
+    color: 'lightseagreen',
+    fontSize:16,
+    padding:20,
   },
 });
